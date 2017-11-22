9160b73dc446bf5f7d68bfb4c59eb56454751361
François Pillot <f.pillot@criteo.com>
2017-11-22 12:06:01 -0500

    Print the build steps to stdout. Reformatting the sync function

--- moon.py
+++ moon.py
@@ -10,8 +10,9 @@ import pathlib
 import sys
 from autologging import traced, logged, TRACE
 
-logging.basicConfig(level=TRACE, stream=sys.stdout,
-    format="%(levelname)s:%(name)s:%(funcName)s:%(message)s")
+#logging.basicConfig(level=TRACE, stream=sys.stdout,
+#    format="%(levelname)s:%(name)s:%(funcName)s:%(message)s")
+logger = logging.getLogger(__name__)
 
 @traced
 @attr.s
@@ -64,7 +65,8 @@ class Image:
                 args.extend([ "-f", self.options['dockerfile'] ])
             if 'args' in self.options:
                 args.extend([ "--build-arg", '"'+" ".join(self.options['args'])+'"' ])
-        sh.docker.build(args)
+        for line in sh.docker.build(args, _iter=True):
+            logger.info(line)
     def push(self):
         sh.docker.push(self.name)
 
@@ -178,14 +180,6 @@ class Swarm:
 class Manager:
     def __attrs_post_init__(self):
         self.swarm = Swarm()
-    def clean_stacks(self, instructions):
-        instructions_stack_names = [ x.name for x in instructions.stacks ]
-        undesired_stacks = [x for x in self.swarm.stacks if x.name not in instructions_stack_names and x.name != 'moon']
-        for stack in undesired_stacks: stack.remove_from(self.swarm)
-    def create_networks(self, instructions):
-        for network in instructions.networks:
-            if not any(running_network.name == network.name for running_network in self.swarm.networks):
-                network.add_to(self.swarm)
     def link(self, url, branch, stacks_filename="moon-stacks.yml"):
         self.instructions = Instructions(
                             Repository(
@@ -193,47 +187,52 @@ class Manager:
                                 url=url, 
                                 branch=branch), 
                             filename=stacks_filename)
+    def clean_stacks(self, instructions):
+        for running_stack in self.swarm.stacks:
+            if running_stack.name != 'moon'\
+            and not any(stack.name == running_stack.name
+                       for stack in instructions.stacks)
+                logger.info('Removing stack {}'.format(stack.name))
+                stack.remove_from(self.swarm)
+    def create_networks(self, instructions):
+        for network in instructions.networks:
+            if not any(running_network.name == network.name 
+                       for running_network in self.swarm.networks):
+                logger.info('Adding network {}'.format(network.name))
+                network.add_to(self.swarm)
+    def build_and_deploy(self, stacks):
+        for stack in stacks:
+            for service in stack.services:
+                if service.repository:
+                    if not service.repository.uptodate():
+                        logger.info(
+                            'Service {} in stack {} not up-to-date. Refreshing...'\
+                            .format(
+                                service.name,
+                                stack.name))
+                        service.image.build()
+                        service.image.push()
+                        stack.add_to(self.swarm)
+                        service.repository.refresh()
     def sync(self):
         if not self.instructions.uptodate():
+            logger.info('Instructions not up-to-date. Refreshing...')
             self.instructions.refresh()
             self.clean_stacks(self.instructions)
             self.create_networks(self.instructions)
-            # Build
-            for stack in self.instructions.stacks:
-                for service in stack.services:
-                    if service.repository:
-                        if not service.repository.uptodate():
-                            service.image.build()
-                            service.image.push()
-                            service.repository.refresh()
+            self.build_and_deploy(self.instructions.stacks)
             # Deploy everything
             for stack in self.instructions.stacks:
                 stack.add_to(self.swarm)
         else:
             self.clean_stacks(self.instructions)
             self.create_networks(self.instructions)
-            # Build
-            for stack in self.instructions.stacks:
-                for service in stack.services:
-                    if service.repository:
-                        if not service.repository.uptodate():
-                            service.image.build()
-                            service.image.push()
-                            service.repository.refresh()
+            self.build_and_deploy(self.instructions.stacks)
             # Deploy only missing
-            if not any(running_stack.name == stack.name for running_stack in self.swarm.stacks): 
-                stack.add_to(self.swarm)
-
-
-        for stack in self.instructions.stacks:
-            for service in stack.services:
-                if service.repository:
-                    if not service.repository.uptodate():
-                        service.image.build()
-                        service.image.push()
-                        stack.add_to(self.swarm)
-                        service.repository.refresh()
-
+            for stack in self.instructions.stacks:
+                if not any(running_stack.name == stack.name 
+                           for running_stack in self.swarm.stacks):
+                    stack.add_to(self.swarm)
     def monitor(self, cycle_time):
         while True:
             self.sync()
